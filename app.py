c7638d80558029bec945e27067a99f44849fb173
KhanShaheb34 <shakirulhkhan@gmail.com>
2025-01-18 12:35:20 +0600

    Implement contact form functionality with validation and message display; update .gitignore to include additional file types; add Flask-WTF form handling in forms.py.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from forms import ContactForm
 
 app = Flask(__name__)
+app.secret_key = "my_secret_key"
+
+messages = []
 
 
 @app.route("/")
@@ -8,9 +12,21 @@ def home():
     return render_template("home.jinja")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.jinja")
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+
+        messages.append(f"{name}: {email} sent: {message}")
+
+        flash(f"Thanks for message {name}", "success")
+        return redirect(url_for("contact"))
+
+    return render_template("contact.jinja", form=form, messages=messages)
 
 
 if __name__ == "__main__":
